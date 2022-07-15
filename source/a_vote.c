aff60c9460243f0512dac68dfb359a5baf185fce
adean <8dino2@gmail.com>
2022-07-15 16:20:22 -0400

    Debugging

--- source/a_vote.c
+++ source/a_vote.c
@@ -188,6 +188,7 @@ static void Votemap(edict_t *ent, const char *mapname)
 	}
 
 	gi.cprintf(ent, PRINT_HIGH, "mapvoteNextTime is %d\n", rsecs);
+	gi.cprintf(ent, PRINT_HIGH, "gametime is %d\n", gametime);
 
 	// If timelimit is set and if mapvote_next is 2, and the remaining time is less than the mapvote_next_time, do not allow the mapvote
 	if (timelimit->value && mapvote_next->value == 2 && rsecs < mapvote_next_time->value){
