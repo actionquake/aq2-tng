57cfd7fd70b9c0d3159b527ebd4b37e6610714fe
Aaron Dean <8dino2@gmail.com>
2023-12-01 14:41:05 -0500

    Fixed a merge bug

--- source/p_view.c
+++ source/p_view.c
@@ -699,7 +699,7 @@ void P_FallingDamage (edict_t * ent)
 			ent->s.event = EV_FALLFAR;
 		else			// all falls are far
 			ent->s.event = EV_FALLFAR;
-		if (e_enhancedSlippers->value && INV_AMMO(ent, SLIP_NUM))
+		if (esp_enhancedslippers->value && INV_AMMO(ent, SLIP_NUM))
 			ent->s.event = EV_FALL;
 	}
 
